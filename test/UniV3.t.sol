268e601c6a8c98d801a84afd7708b0e6d04a629b
zeroXbrock <2791467+zeroXbrock@users.noreply.github.com>
2025-01-24 14:11:28 -0800

    uhm

--- test/UniV3.t.sol
+++ test/UniV3.t.sol
@@ -11,7 +11,7 @@ import {SwapRouter} from "lib/v3-periphery/contracts/SwapRouter.sol";
 import {NonfungibleTokenPositionDescriptor} from "lib/v3-periphery/contracts/NonfungibleTokenPositionDescriptor.sol";
 import {NonfungiblePositionManager} from "lib/v3-periphery/contracts/NonfungiblePositionManager.sol";
 
-contract CounterTest is Test {
+contract UniV3Test is Test {
     TestToken public token1;
     TestToken public token2;
     WETH9 public weth;
@@ -20,19 +20,26 @@ contract CounterTest is Test {
     NonfungibleTokenPositionDescriptor public tokenDescriptor;
     NonfungiblePositionManager public positionManager;
 
-    function setUp() public {
-        // CREATE
+    function setupCreate_1() public {
         token1 = new TestToken(1000000 ether);
         token2 = new TestToken(1000000 ether);
         weth = new WETH9();
         factory = new UniswapV3Factory();
-        router = new SwapRouter(address(factory), address(weth));
-        tokenDescriptor = new TokenDescriptor();
-        positionManager = new NonfungiblePositionManager(
-            address(factory),
-            address(weth),
-            address(tokenDescriptor)
-        );
+        // router = new SwapRouter(address(factory), address(weth));
+    }
+
+    function setUp() public {
+        // CREATE
+        setupCreate_1();
+        // tokenDescriptor = new NonfungibleTokenPositionDescriptor(
+        //     address(weth),
+        //     0x0000000000000000000000000000000000000000000000000000000057455448
+        // );
+        // positionManager = new NonfungiblePositionManager(
+        //     address(factory),
+        //     address(weth),
+        //     address(tokenDescriptor)
+        // );
 
         // SETUP
         weth.deposit{value: 10 ether}();
